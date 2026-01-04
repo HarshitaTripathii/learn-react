8fd20603171b6d8800accec681640869ed38d276
HarshitaTripathii <harshitat216@gmail.com>
2026-01-05 00:56:36 +0530

    day3 done

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -1,35 +1,69 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const [name , setName] = useState("")
+  // const [age , setAge] = useState(0)
+  // const [data, setData]=useState([{
+  //   name: "Harshit",
+  //   age: 20
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // }, {
+  //   name: "Harshita",
+  //   age: 22
+  // }])
+  // console.log(name, age);
+  
+  // console.log(data);
+  // console.log(...data);
+  // return (
+  //   <div>
+  //     <form onSubmit={e => {
+  //       e.preventDefault()
+  //       setData([...data, {
+  //         name:name,
+  //         age:age
+  //       }])
+  //       console.log("submitted")
+  //     }}>
+  //       <input type='text' onChange={e => setName(e.target.value)} />
+  //       <input type='number'onChange={e => setAge(e.target.value)} />
+  //       <button type='submit'>Submit</button>
+  //     </form>
+
+  //     {data.map(item => (
+  //       <Card name={item.name} age={item.age} />
+  //     ))}
+
+  //   </div>
+  // )
+  const [data, setData]=useState(null);
+  const[loading, setLoading]=useState(false);
+
+  useEffect(()=>{
+     async function fetchData(){
+      setLoading(true);
+      const response=await fetch('https://jsonplaceholder.typicode.com/users')
+      const data1=await response.json();
+      console.log(data1);
+      setData(data1);
+      setLoading(false);
+
+  }
+  fetchData();
+ 
+  }, [])
+  console.log("Data", data)
+  console.log(loading)
+  if(loading) return <div><h1>Loading</h1></div>
+  return <div>
+    {data?.map(item => (
+          <Card name={item.name} age={item.email} />
+    ))}
+  </div>
 }
 
 export default App
---
+++ day3/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card({name,age}) {
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>{age}</p>
+
+        </div>
+    )
+}
+
+export default Card
