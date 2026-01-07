9bfeae623573b725addddadaed564367ece442ff
HarshitaTripathii <harshitat216@gmail.com>
2026-01-07 23:01:19 +0530

    day4 delete task done

--- day4/src/components/Todo.jsx
+++ day4/src/components/Todo.jsx
@@ -3,39 +3,47 @@ import { useState } from 'react'
 
 function Todo() {
     const [todo, setTodo] = useState([]);
-    const [currentTodo, setCurrentTodo]=useState("")
-     
+    const [currentTodo, setCurrentTodo] = useState("")
+
     function addTask(e) {
         e.preventDefault();
         setTodo([...todo, { task: currentTodo, checked: false }])
-        
+
     }
+    
     console.log(todo);
     return (
         <div>
             <form onSubmit={addTask}>
                 <input type='text' placeholder='add task' value={currentTodo} onChange={e => {
                     setCurrentTodo(e.target.value)
-                    console.log(currentTodo)}}/>
+                    console.log(currentTodo)
+                }} />
                 <button>Add</button>
             </form>
 
             <div>
                 {todo.map((item, index) => (
-                    
+
                     <div>
                         <p>{item.task}</p>
-                        <button onClick={() => {
-                            const newArray=[...todo]
-                            newArray[index].checked=!(item.checked)
-                            setTodo(newArray )
-                        }}>{item.checked? "Checked" : "Unchecked"}</button>
-                    </div>
-                    
-                ))}
-            </div>
 
+                        <button onClick={() => {
+                            const newArray = [...todo]
+                            newArray[index].checked = !(item.checked)
+                            setTodo(newArray)
+                        }}>{item.checked ? "Checked" : "Unchecked"}</button>
+                        <button
+                            onClick={() => {
+                                const newA= todo.filter((ele, i) => i != index );
+                                setTodo(newA);
+                            }}>Delete</button>
+                    </div>
+
+                ))}
         </div>
+
+        </div >
 
     )
 }
